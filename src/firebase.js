fda45f616c8eb99779e50cde41db06bdfa52141c
Кайрат <110962052+Kairat-Nobels@users.noreply.github.com>
2023-10-06 17:53:11 +0600

    change api

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDx1hqMwAKxJaPx3HpFE0p1I3RSM3-Ujvk",
-    authDomain: "chat-a513c.firebaseapp.com",
-    projectId: "chat-a513c",
-    storageBucket: "chat-a513c.appspot.com",
-    messagingSenderId: "189529625141",
-    appId: "1:189529625141:web:6626484932c4a8e567577c"
+    apiKey: "AIzaSyC6XaYudpPoUGoHDb0Tl57O6-qR4O3w1iQ",
+    authDomain: "chat-faf27.firebaseapp.com",
+    projectId: "chat-faf27",
+    storageBucket: "chat-faf27.appspot.com",
+    messagingSenderId: "45792056292",
+    appId: "1:45792056292:web:81abfd9d965af2179eedf1"
 };
 
 // Initialize Firebase
